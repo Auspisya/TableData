3dead633866ce3e84c042039d4b9b1982a98a654
Lyubov Menshakova <menshakova222@gmail.com>
2023-01-30 16:07:49 +0300

    Реализована возможность снять бронь и выбрать услугу. Не реализованы обработчики ошибок.

---
+++ TableData/Models/Table.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableData.Models
+{
+    class Table
+    {
+        private int Number;
+        private int MaxPlaces;
+        private int FreePlaces;
+
+        public Table(int number, int maxPlaces)
+        {
+            Number = number;
+            MaxPlaces = maxPlaces;
+            FreePlaces = maxPlaces;
+        }
+
+        public void ShowInfo() => Console.WriteLine($"Стол {Number}. Свободно мест {FreePlaces} из {MaxPlaces}.");
+
+        #region Допфункции
+        public bool Reserve(int places)
+        {
+            if (FreePlaces >= places)
+            {
+                FreePlaces -= places;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool TakeOffReserve(int places)
+        {
+            if (places >= FreePlaces && places <= MaxPlaces)
+            {
+                FreePlaces += places;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ TableData/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableData.Models;
+
+namespace TableData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOpenCafe = true;
+
+            Table[] tables =
+            {
+                new Table(1,4), //0
+                new Table(2,7), //1
+                new Table(3,10) //2
+            };
+
+                    //true
+            while (isOpenCafe)
+            {
+                Console.WriteLine("Админ-панель");
+                for (int i = 0; i < tables.Length; i++)
+                {
+                    tables[i].ShowInfo();
+                }
+
+                #region Допфункции
+
+                Console.Write("\nУслуги:");
+                Console.Write("\n1.Забронировать стол.");
+                Console.Write("\n2.Снять бронь со стола.");
+                int firstService = 1;
+                Console.Write("\nВведите цифру услуги:");
+                if (firstService == Convert.ToInt32(Console.ReadLine())) {
+                    Console.Write("\nВведите номер стола:");
+                    int wishTable = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                    Console.Write("\nВведите количество лиц для брони:");
+                    int getPlaces = Convert.ToInt32(Console.ReadLine());
+
+                    bool isReComplete = tables[wishTable].Reserve(getPlaces);
+
+                    if (isReComplete) Console.WriteLine("Прошло успешно");
+                    else Console.WriteLine("Бронь не прошла");
+
+                    Console.ReadKey();
+                    Console.Clear();
+                } else { 
+                    Console.Write("\nВведите номер стола:");
+                    int wishTable = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                    Console.Write("\nВведите количество лиц для снятия брони:");
+                    int getPlaces = Convert.ToInt32(Console.ReadLine());
+
+                    bool isReComplete = tables[wishTable].TakeOffReserve(getPlaces);
+
+                    if (isReComplete) Console.WriteLine("Прошло успешно");
+                    else Console.WriteLine("Снятие брони не прошло");
+
+                    Console.ReadKey();
+                    Console.Clear();
+                
+                }
+                #endregion
+
+            }
+        }
+    }
+    
+}
